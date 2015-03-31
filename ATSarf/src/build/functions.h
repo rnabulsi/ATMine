5c5aacb2f119cda8ec7c0e5cd348fc9c7305ad87
Toplica Tanaskovic <toplicius@gmail.com>
2015-03-31 23:46:03 +0200

    Cleaned up ATSarf/src/build directory.

--- ATSarf/src/build/functions.h
+++ ATSarf/src/build/functions.h
@@ -1,11 +1,10 @@
 #ifndef INSERT_SCRIPTS_H
 #define INSERT_SCRIPTS_H
 
-
-//needed when adding names with Al
+// needed when adding names with Al
 int insert_rules_for_Nprop_Al();
 
-//main functions
+// main functions
 int insert_buckwalter();
 
 int insert_propernames();
--- ATSarf/src/cache/database_info_block.h
+++ ATSarf/src/cache/database_info_block.h
@@ -1,7 +1,8 @@
 /**
   * @file   database_info_block.h
   * @author Jad Makhlouta and documented by Ameen Jaber
-  * @brief  This file implements the database_info_block class as well as some other types required for database extraction
+  * @brief  This file implements the database_info_block class as well as some other types required for database
+ * extraction
   * and data manipulation
   */
 #ifndef DATABASE_INFO_H
@@ -20,11 +21,11 @@
 /// Input triplet to hash table (item_id,category_id,raw_data)
 typedef Triplet<long long, long, QString> ItemEntryKey;
 /// Output triplet from the hash table (abstract_categories or reverse_description,description_id,POS)
-typedef Triplet<dbitvec,long,QString> ItemEntryInfo;
+typedef Triplet<dbitvec, long, QString> ItemEntryInfo;
 /// Hash table that takes ItemEntry triplet and returns ItemEntryInfo triplet
-typedef QMultiHash<ItemEntryKey,ItemEntryInfo > ItemCatRaw2AbsDescPosMap;
+typedef QMultiHash<ItemEntryKey, ItemEntryInfo> ItemCatRaw2AbsDescPosMap;
 /// Type definition of a pointer to a triplet to triplet hash function
-typedef ItemCatRaw2AbsDescPosMap * ItemCatRaw2AbsDescPosMapPtr;
+typedef ItemCatRaw2AbsDescPosMap *ItemCatRaw2AbsDescPosMapPtr;
 /// Type definition of an iterator over a hash function
 typedef ItemCatRaw2AbsDescPosMap::iterator ItemCatRaw2AbsDescPosMapItr;
 
@@ -35,36 +36,35 @@ typedef ItemCatRaw2AbsDescPosMap::iterator ItemCatRaw2AbsDescPosMapItr;
   * @author Jad Makhlouta and documented by Ameen Jaber
   */
 class database_info_block {
-	private:
-		void readTrieFromDatabaseAndBuildFile();
-		void buildTrie();
-		void buildDescriptions();
-		void readDescriptionsFromDatabaseAndBuildFile();
-		void buildMap(item_types type,ItemCatRaw2AbsDescPosMap * map);
-		void fillMap(item_types type,ItemCatRaw2AbsDescPosMap * map);
-    public:
-		ATMProgressIFC *prgsIFC;
+private:
+    void readTrieFromDatabaseAndBuildFile();
+    void buildTrie();
+    void buildDescriptions();
+    void readDescriptionsFromDatabaseAndBuildFile();
+    void buildMap(item_types type, ItemCatRaw2AbsDescPosMap *map);
+    void fillMap(item_types type, ItemCatRaw2AbsDescPosMap *map);
 
-        tree* Prefix_Tree;
-        tree* Suffix_Tree;
-#ifdef USE_TRIE
-		ATTrie * Stem_Trie;
-		StemNodesList * trie_nodes;
-#endif
-		compatibility_rules * comp_rules;
+public:
+    ATMProgressIFC *prgsIFC;
 
-                /// Triplet Hash for the prefix
-                ItemCatRaw2AbsDescPosMapPtr map_prefix;
-                /// Triplet Hash for the stem
-                ItemCatRaw2AbsDescPosMapPtr map_stem;
-                /// Triplet Hash for the suffix
-                ItemCatRaw2AbsDescPosMapPtr map_suffix;
+    tree *Prefix_Tree;
+    tree *Suffix_Tree;
+    ATTrie *Stem_Trie;
+    StemNodesList *trie_nodes;
+    compatibility_rules *comp_rules;
 
-		QVector<QString>* descriptions;
+    /// Triplet Hash for the prefix
+    ItemCatRaw2AbsDescPosMapPtr map_prefix;
+    /// Triplet Hash for the stem
+    ItemCatRaw2AbsDescPosMapPtr map_stem;
+    /// Triplet Hash for the suffix
+    ItemCatRaw2AbsDescPosMapPtr map_suffix;
 
-        database_info_block();
-		void fill(ATMProgressIFC *p_ifc);
-        ~database_info_block();
+    QVector<QString> *descriptions;
+
+    database_info_block();
+    void fill(ATMProgressIFC *p_ifc);
+    ~database_info_block();
 };
 
 extern database_info_block database_info;
